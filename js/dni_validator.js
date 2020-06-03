8fd56693b1914b7442e551424fb6c509617d7286
Tomeu Barcelo <tomeu.95@gmail.com>
2020-06-03 19:06:52 +0200

    Mejoras en la validacion

--- js/dni_validator.js
+++ js/dni_validator.js
@@ -2,38 +2,47 @@ const letterDni = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', '
 
 function validaDNI() {
     console.log('click en comprobar dni');
-    var dni = document.getElementsByName("dni")[0].value;
+    var dni = document.getElementsByName("dni")[0].value; //valor del input 
     console.log("DNI introducido: " + dni);
 
-    var dniSoloNum = dni.slice(0, 8); //eliminamos la letra introducida
-    console.log("DNI sin la letra: " + dniSoloNum);
-
-    var result = dniSoloNum % 23; //calculamos el resto del dni introducido entre 23 (num de letras)
-    console.log('El resultado de ' + dniSoloNum + ' % 23 es: ' + result);
-
-    var letraSeleccionada = letterDni[result]; //segun el resto obtenido buscaremos en el array dicha posicion
-    console.log('La letra correspondiente es: ' + letraSeleccionada);
-    console.log('Por tanto tu DNI es: ' + dniSoloNum + letraSeleccionada);
-
-    var dniAndLetter = dniSoloNum + letraSeleccionada;
-    console.log("El DNI correcto seria: " + dniAndLetter);
-
-
-    //mostrar datos pantalla
-    var nombre = document.getElementsByName("nombre")[0].value;
-    var apellidos = document.getElementsByName("apellidos")[0].value;
-    document.getElementById("resultNameConApellidos").innerHTML = nombre + " " + apellidos;
-
-    if (dni === dniAndLetter) {
-        console.log('Es correcto');
-        document.getElementById("resultFormDni").innerHTML = dni + " es correcto";
-        document.getElementById("resultFormDni").style.color = "green";
+    console.log("Longitud dni: " + dni.length);
+    if (dni.length != 9) { //si la longitud del dni es diferete de 9 
+        alert("Debe poner 8 números y una letra");
+        document.getElementById("resultNameConApellidos").innerHTML = "";
+        document.getElementById("resultFormDni").innerHTML = "";
         document.getElementById("resultFormDniQueTocaria").innerHTML = "";
+        document.getElementsByName("dni")[0].style.border = "2px solid red";
     } else {
-        console.log('Es incorrecto');
-        document.getElementById("resultFormDni").innerHTML = dni + " es incorrecto!";
-        document.getElementById("resultFormDni").style.color = "red";
-        document.getElementById("resultFormDniQueTocaria").innerHTML = "El DNI correcto sería: " + dniAndLetter;
-    }
+        var dniSoloNum = dni.slice(0, 8); //eliminamos la letra introducida
+        console.log("DNI sin la letra: " + dniSoloNum);
 
+        var result = dniSoloNum % 23; //calculamos el resto del dni introducido entre 23 (num de letras)
+        console.log('El resultado de ' + dniSoloNum + ' % 23 es: ' + result);
+
+        var letraSeleccionada = letterDni[result]; //segun el resto obtenido buscaremos en el array dicha posicion
+        console.log('La letra correspondiente es: ' + letraSeleccionada);
+
+        var dniAndLetter = dniSoloNum + letraSeleccionada; //juntamos los 8 digitos con la letra que toca
+        console.log("El DNI correcto seria: " + dniAndLetter);
+
+
+        //mostrar datos pantalla
+        var nombre = document.getElementsByName("nombre")[0].value; //valor del input nombre
+        var apellidos = document.getElementsByName("apellidos")[0].value; //apellidos
+        document.getElementById("resultNameConApellidos").innerHTML = nombre + " " + apellidos;
+
+        document.getElementsByName("dni")[0].style.border = "";
+
+        if (dni === dniAndLetter) { //si el dni introducido es igual que el dni que tocaria con esos digitos
+            console.log('Es correcto');
+            document.getElementById("resultFormDni").innerHTML = dni + " es correcto";
+            document.getElementById("resultFormDni").style.color = "green";
+            document.getElementById("resultFormDniQueTocaria").innerHTML = "";
+        } else { //caso contrario
+            console.log('Es incorrecto');
+            document.getElementById("resultFormDni").innerHTML = dni + " es incorrecto!";
+            document.getElementById("resultFormDni").style.color = "red";
+            document.getElementById("resultFormDniQueTocaria").innerHTML = "El DNI correcto sería: " + dniAndLetter;
+        }
+    }
 };
